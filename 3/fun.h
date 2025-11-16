e8d948851fc87ac31fd9aaa8c716699ad0c1373d
dreqbarqn <thevoorhiz@gmail.com>
2025-11-16 13:18:06 +0300

    вроде конфликты убрал ненужное удалил

--- 3/fun.h
+++ 3/fun.h
@@ -1,20 +1,8 @@
-<<<<<<< HEAD
-#ifndef INPUT_H
-#define INPUT_H
-void vstavka();
-void udalenie();
-int* individ();
-#endif 
-=======
 #ifndef FUN_H
 #define FUN_H
-
-#include <stdbool.h>
 
 void vstavka(int **a, int *n);
 void udalenie(int **a, int *n, int k);
 int *individ(int **a, int *n);
 
 #endif
-
->>>>>>> 58e4756 (Сохранение текущих изменений перед pull)
--- 3/proverk.h
+++ 3/proverk.h
@@ -6,7 +6,6 @@ int inputcheck_value(int *n);
 int inputcheck_index_i(int *index, int *n);
 int inputcheck_index_d(int *index, int *n);
 int inputcheck_n(int *n);
+void safe_exit(int**x);
 
 #endif
-
->>>>>>> 58e4756 (Сохранение текущих изменений перед pull)
